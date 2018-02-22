cb3356339dc3d3edcd4bb1d778c9761529826b2d
Meidongwei <450650472@qq.com>
2018-02-22 17:42:50 +0800

    update url

--- src/http_url.js
+++ src/http_url.js
@@ -1,6 +1,6 @@
 let localhostDev = false
 
-let _host= localhostDev ? 'http://localhost:8080' : '/pzcatering-web/crmwxapi.do?method='
+let _host= localhostDev ? 'http://localhost:8080' : '/pzcatering-web/crmwx/api.do?method='
 // let _host = localhostDev ? 'http://localhost:8080' : 'http://192.168.1.160:8081/pzcatering-web/crmwxapi.do?method='
 // 公网
 // let _host = localhostDev ? 'http://localhost:8080' : 'http://139.129.222.149:13020/pzcatering-web/crmwxapi.do?method='
