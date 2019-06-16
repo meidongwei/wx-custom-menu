1c887a3e23b7e9585e33a90e1b0b1fd586acc44e
Meidongwei <450650472@qq.com>
2019-06-16 15:08:04 +0800

    Update http_url.js

--- src/http_url.js
+++ src/http_url.js
@@ -1,15 +1,11 @@
-let localhostDev = false
+let localhostDev = true
 
 let _host= localhostDev ? 'http://localhost:8080' : '/pzcatering-web/crmwx/api.do?method='
-// let _host = localhostDev ? 'http://localhost:8080' : 'http://192.168.1.160:8081/pzcatering-web/crmwxapi.do?method='
-// 公网
-// let _host = localhostDev ? 'http://localhost:8080' : 'http://139.129.222.149:13020/pzcatering-web/crmwxapi.do?method='
+// _host = localhostDev ? 'http://localhost:8080' : 'http://192.168.1.160:8081/pzcatering-web/crmwxapi.do?method='
+// _host = localhostDev ? 'http://localhost:8080' : 'http://139.129.222.149:13020/pzcatering-web/crmwxapi.do?method='
 let httpUrl = {
-  // 初始化页面时加载菜单信息
-  'getDatas': _host + 'getBrandMenuJson',
-  // 点击保存时触发
-  'handleSave': _host + 'setBrandMenuJson',
-  // 初始化页面时加载 “图文信息” 列表
-  'getBrandMaterialList': _host + 'getBrandMaterialList'
+  'getDatas': _host + 'getBrandMenuJson', // 初始化页面时加载菜单信息
+  'handleSave': _host + 'setBrandMenuJson', // 点击保存时触发
+  'getBrandMaterialList': _host + 'getBrandMaterialList', // 初始化页面时加载 “图文信息” 列表
 };
 export default httpUrl
